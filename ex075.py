d4dff0f70b33bbc59d5d6627e8f3a361dba6ae08
Pedro Rafael <pedroorafav@gmail.com>
2023-03-03 09:52:14 -0300

    update 03/03

--- ex075.py
+++ ex075.py
@@ -7,8 +7,19 @@ n1 = int(input('Escreva um número: '))
 n2 = int(input('Escreva outro número: '))
 n3 = int(input('Escreva outro número: '))
 n4 = int(input('Escreva outro número: '))
-numeros = n1, n2, n3, n4
-print(numeros)
-print(f'o número 9 apareceu {numeros.count(9)} vezes na tupla')
-print(f'O valor 3 foi encontrado na tupla na posição {numeros.index(3)}')
-    ###falta terminar!!!!
+num = n1, n2, n3, n4
+print(num)
+print(f'o número 9 apareceu {num.count(9)} vezes na tupla')
+if num.count(3) == 0:
+    print('O valor 3 não foi encontrado na tupla')
+else:
+    print(f'O valor de 3 foi encontrado na posição {num.index(3)}')
+print('Os números pares foram: ')
+if n1 % 2 == 0:
+    print(n1, end=', ')
+if n2 % 2 == 0:
+    print(n2, end=', ')
+if n3 % 2 == 0:
+    print(n3, end=', ')
+if n4 % 2 == 0:
+    print(n4, end='')
