c3fad81723cbf1d812d7cecc0b69a9a3776891a1
Pedro Rafael <pedroorafav@gmail.com>
2023-03-03 00:22:19 -0300

    update 03/03

--- ex073.py
+++ ex073.py
@@ -3,3 +3,9 @@
 #b - os ultimos 4 colocados
 #c - uma lista com o time em ordem alfabetica
 #d - em que colocação esta o time da chapecoense
+times = 'América-MG', 'Athletico-Pr', 'Atlético-MG', 'Bahia', 'Botafogo', 'Bragantino', 'Corinthians', 'Coritiba', 'Cruzeiro', 'Cuiabá', 'Flamengo', 'Fluminense', 'Fortaleza', 'Goiás', 'Gremio', 'Internacional', 'Palmeiras', 'Santos', 'Vasco'
+print(f'Classificação dos {len(times)+ 1} primeiros times do Campeonato Brasileiro Série A')
+print(f'Os 5 primeiros colocados são {times[0:5]}')
+print(f'Os quatro ultimos colocados foram {times[-4:]}')
+print('Segue a baixo a lista em ordem alfabética: ')
+print(sorted(times))
--- ex075.py
+++ ex075.py
@@ -2,3 +2,13 @@
 #mostre quantas vezes apareceu o valor 9
 #em que posição foi digitado o primeiro valor 3
 #Quais foram os números pares
+import math
+n1 = int(input('Escreva um número: '))
+n2 = int(input('Escreva outro número: '))
+n3 = int(input('Escreva outro número: '))
+n4 = int(input('Escreva outro número: '))
+numeros = n1, n2, n3, n4
+print(numeros)
+print(f'o número 9 apareceu {numeros.count(9)} vezes na tupla')
+print(f'O valor 3 foi encontrado na tupla na posição {numeros.index(3)}')
+    ###falta terminar!!!!
