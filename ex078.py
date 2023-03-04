2b23cd24ac5b7c853e4bb6d3f70b7d99ceecbe7d
Pedro Rafael <pedroorafav@gmail.com>
2023-03-03 23:10:37 -0300

    update3 03/03

---
+++ ex078.py
@@ -0,0 +1,8 @@
+#faça um programa que leia 5 valores numericos e guarde na lista
+#mostre qual foi o maior e o menor e suas respectivas posições na lista
+valores = list()
+for cont in range(0,5):
+    valores.append(int(input(f'Digite um valor para a posição {cont}: ')))
+print(f'Os valores digitados foram {valores}')
+print(f'O maior valor digitado foi {max(valores)} nas posições {valores.index(max(valores))}...')
+print(f'O menor valor digitado foi {min(valores)} nas posições {valores.index(min(valores))}...')
